9e7ca7129a51e3b2512bc86bdf20dbff2323abce
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-02-07 10:43:46 -0800

    Print version

--- omnichannel-chat-sdk/react-botframework-webchat-control/src/App.tsx
+++ omnichannel-chat-sdk/react-botframework-webchat-control/src/App.tsx
@@ -1,11 +1,14 @@
 import { useCallback, useEffect, useState } from 'react'
 import { OmnichannelChatSDK } from '@microsoft/omnichannel-chat-sdk';
+import { version } from '@microsoft/omnichannel-chat-sdk/package.json';
 import './App.css'
 
 function App() {
   const [chatSDK, setChatSDK] = useState<OmnichannelChatSDK>();
 
   useEffect(() => {
+    console.log(`omnichannel-chat-sdk@${version}`);
+
     const omnichannelConfig = {
       orgId: '',
       orgUrl: '',
@@ -27,6 +30,7 @@ function App() {
 
   const startChat = useCallback(async () => {
     await chatSDK?.startChat();
+    console.log("Chat started!");
     await chatSDK?.onNewMessage((message: any) => {
       console.log(`New message!`)
       console.log(message?.content);
