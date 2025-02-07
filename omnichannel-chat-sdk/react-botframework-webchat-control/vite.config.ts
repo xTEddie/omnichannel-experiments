a23bb115f1d75aa5fb73817f13a69bb95e5d8cd1
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-02-06 18:56:04 -0800

    Add alias for acs adapter & fix import issue

--- omnichannel-chat-sdk/react-botframework-webchat-control/vite.config.ts
+++ omnichannel-chat-sdk/react-botframework-webchat-control/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'acs_webchat-chat-adapter': path.resolve(__dirname, 'node_modules', 'acs_webchat-chat-adapter', 'dist', 'chat-adapter.js')
+    }
+  }
 })
