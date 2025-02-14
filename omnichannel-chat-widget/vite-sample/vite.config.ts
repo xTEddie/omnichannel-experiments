f32704ab11e60da6e21aa0a5587e8f109ea54d86
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-02-13 21:15:11 -0800

    Resolve packages

--- omnichannel-chat-widget/vite-sample/vite.config.ts
+++ omnichannel-chat-widget/vite-sample/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias:     {
+      '@microsoft/botframework-webchat-adapter-azure-communication-chat': path.resolve(__dirname, 'node_modules', '@microsoft', 'botframework-webchat-adapter-azure-communication-chat', 'dist', 'chat-adapter.js'),
+      '@fluentui/react': path.resolve(__dirname, 'node_modules', '@microsoft', 'omnichannel-chat-components', 'node_modules', '@fluentui', 'react')
+    }
+  }
 })
