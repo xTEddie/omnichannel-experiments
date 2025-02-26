0c2636d1ac500c4f3f20e566a4049b0543cf677c
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-02-25 17:43:19 -0800

    Load ReactWebChat component on starting chat

--- omnichannel-chat-sdk/react-botframework-webchat-control/src/App.tsx
+++ omnichannel-chat-sdk/react-botframework-webchat-control/src/App.tsx
@@ -1,6 +1,9 @@
 import { useCallback, useEffect, useState } from 'react'
 import { OmnichannelChatSDK } from '@microsoft/omnichannel-chat-sdk';
-import { version } from '@microsoft/omnichannel-chat-sdk/package.json';
+import ReactWebChat from 'botframework-webchat';
+import { version as chatSDKversion } from '@microsoft/omnichannel-chat-sdk/package.json';
+import { version as webChatVersion } from 'botframework-webchat/package.json';
+import { version as chatAdapterVersion } from '@microsoft/botframework-webchat-adapter-azure-communication-chat/package.json';
 import fetchDebugConfig from './utils/fetchDebugConfig';
 import fetchOmnichannelConfig from './utils/fetchOmnichannelConfig';
 import fetchChatSDKConfig from './utils/fetchChatSDKConfig';
@@ -8,9 +11,12 @@ import './App.css';
 
 function App() {
   const [chatSDK, setChatSDK] = useState<OmnichannelChatSDK>();
+  const [chatAdapter, setChatAdapter] = useState<any>(undefined);
 
   useEffect(() => {
-    console.log(`omnichannel-chat-sdk@${version}`);
+    console.log(`omnichannel-chat-sdk@${chatSDKversion}`);
+    console.log(`botframework-webchat@${webChatVersion}`);
+    console.log(`botframework-webchat-adapter-azure-communication-chat@${chatAdapterVersion}`)
     const omnichannelConfig = fetchOmnichannelConfig();
     const debugConfig = fetchDebugConfig();
     const chatSDKConfig = fetchChatSDKConfig();
@@ -39,6 +45,9 @@ function App() {
       console.log(`New message!`)
       console.log(message?.content);
     });
+
+    const chatAdapter = await chatSDK?.createChatAdapter();
+    setChatAdapter(chatAdapter);
   }, [chatSDK]);
 
   const endChat = useCallback(async () => {
@@ -55,6 +64,11 @@ function App() {
         <button onClick={endChat}>
           End Chat
         </button>
+      </div>
+      <div style={{position: 'absolute', bottom: 20, right: 20, height: 560, width: 350}}>
+        {chatAdapter && <ReactWebChat
+          directLine={chatAdapter}
+        />}
       </div>
     </>
   )
