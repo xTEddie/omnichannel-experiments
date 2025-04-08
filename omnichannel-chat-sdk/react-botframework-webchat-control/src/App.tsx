90d297d773dc96607d999c9196e9a748f6a48719
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-04-08 13:18:21 -0700

    Clean up

--- omnichannel-chat-sdk/react-botframework-webchat-control/src/App.tsx
+++ omnichannel-chat-sdk/react-botframework-webchat-control/src/App.tsx
@@ -112,9 +112,10 @@ function App() {
       { hasChatStarted && <div style={{position: 'absolute', bottom: 20, right: 20, height: 560, width: 350, border: '1px solid rgb(209, 209, 209)', display: 'flex', flexDirection: 'column'}}>
           <ChatHeader onClose={endChat}/>
           {chatAdapter && <ReactWebChat
-            directLine={chatAdapter}
-            activityMiddleware={createActivityMiddleware()}
-          />}
+              directLine={chatAdapter}
+              activityMiddleware={createActivityMiddleware()}
+            />
+          }
         </div>
       }
     </>
