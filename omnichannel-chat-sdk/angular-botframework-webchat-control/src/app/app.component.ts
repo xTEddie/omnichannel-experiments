d3f52adda5b5343148878c27e41212f3dabd3dd0
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-04-10 01:51:22 -0700

    Load WebChat lib in App

--- omnichannel-chat-sdk/angular-botframework-webchat-control/src/app/app.component.ts
+++ omnichannel-chat-sdk/angular-botframework-webchat-control/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WebChatControlService } from './webchat-control.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-botframework-webchat-control';
+  webChat: any = null;
+
+  constructor(
+    private readonly webChatControlService: WebChatControlService, @Inject(DOCUMENT) private readonly document: any
+  ) {}
 }
