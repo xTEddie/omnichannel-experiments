431668b36f348eb87a2cda100017f0f40c897778
xTEddie <edtran@microsoft.com>
2025-05-06 14:37:29 -0700

    Vite Sample (#16)
    * Add util to fetch chat sdk config
    
    * Pass ChatSDKConfig to ChatSDK
    
    * Add util to fetch auth token
    
    * Pass authToken to chat widget if any
    
    * Add chat commands component
    
    * Render chat commands component
    
    * Add app config
    
    * Console log auth token based on app config
    
    * Update styling
    
    * Update interface
    
    * Hide chat button based on app config
    
    * Add app details component
    
    * Update vite config
    
    * Render app details component
    
    * Remove StrictMode
    
    * Add key
    
    * Fix indentations
    
    * Fix typo

--- omnichannel-chat-sdk/react-botframework-webchat-control/src/utils/fetchAuthToken.ts
+++ omnichannel-chat-sdk/react-botframework-webchat-control/src/utils/fetchAuthToken.ts
@@ -1,27 +1,27 @@
 import AppConfig from "../configs/AppConfig";
 
 interface FetchAuthTokenOptions {
-    option: 'none' | 'local' | 'api'
+  option: 'none' | 'local' | 'api'
 };
 
 const fetchAuthToken = async (options: FetchAuthTokenOptions) => {
-    let authToken = '';
-    if (options.option === 'none') {
-        return '';
-    }
+  let authToken = '';
+  if (options.option === 'none') {
+    return '';
+  }
 
-    if (options.option === 'local') {
-        authToken = import.meta.env.VITE_AUTH_TOKEN || '';
-    }
+  if (options.option === 'local') {
+    authToken = import.meta.env.VITE_AUTH_TOKEN || '';
+  }
 
-    if (options.option === 'api') {
-        // authToken = await fetchAuthTokenViaApi();
-    }
+  if (options.option === 'api') {
+    // authToken = await fetchAuthTokenViaApi();
+  }
 
-    AppConfig.ChatSDK.authToken.log && console.log(`[fetchAuthToken]`);
-    AppConfig.ChatSDK.authToken.log && console.log(authToken);
+  AppConfig.ChatSDK.authToken.log && console.log(`[fetchAuthToken]`);
+  AppConfig.ChatSDK.authToken.log && console.log(authToken);
 
-    return authToken;
+  return authToken;
 };
 
 export default fetchAuthToken;
--- omnichannel-chat-widget/vite-sample/src/main.tsx
+++ omnichannel-chat-widget/vite-sample/src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Note: botframework-webchat is not compatible with StrictMode
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
