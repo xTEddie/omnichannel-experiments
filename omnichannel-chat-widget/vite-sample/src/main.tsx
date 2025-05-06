51c51d6967d4d75574e2f73e18bac389ccd30703
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-05-06 14:37:09 -0700

    Fix typo

--- omnichannel-chat-widget/vite-sample/src/main.tsx
+++ omnichannel-chat-widget/vite-sample/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// Note: botframework-webchat is not compatibile with StrictMode
+// Note: botframework-webchat is not compatible with StrictMode
 createRoot(document.getElementById('root')!).render(
   <App />
 )
