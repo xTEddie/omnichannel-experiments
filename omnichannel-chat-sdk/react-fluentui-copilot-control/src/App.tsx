fe451d08c77969c8c319870d2ed87af5303e7925
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-06-10 09:09:27 -0700

    Render chat commands component & connect buttons with ChatSDK

--- omnichannel-chat-sdk/react-fluentui-copilot-control/src/App.tsx
+++ omnichannel-chat-sdk/react-fluentui-copilot-control/src/App.tsx
@@ -1,16 +1,21 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { OmnichannelChatSDK } from '@microsoft/omnichannel-chat-sdk';
 import AppConfig from './configs/AppConfig';
 import AppDetails from './components/AppDetails/AppDetails';
+import ChatCommands from './components/ChatCommands/ChatCommands';
 import fetchOmnichannelConfig from './utils/fetchOmnichannelConfig';
 import './App.css'
 
 function App() {
+  const [chatSDK, setChatSDK] = useState<OmnichannelChatSDK>();
+
   useEffect(() => {
     const init = async () => {
       const omnichannelConfig = fetchOmnichannelConfig();
       const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
       await chatSDK.initialize();
+
+      setChatSDK(chatSDK);
 
       const chatConfig = await chatSDK.getLiveChatConfig();
       if (AppConfig.ChatSDK.liveChatConfig.log) {
@@ -21,10 +26,44 @@ function App() {
     init();
   }, []);
 
+  const startChat = useCallback(async () => {
+    if (!chatSDK) {
+      return;
+    }
+
+    try {
+      await chatSDK.startChat();
+    } catch (error: any) {
+      console.error(error);
+    }
+
+    await chatSDK?.onNewMessage((message: any) => {
+      AppConfig.ChatSDK.onNewMessage.log && console.log(`New message!`);
+      AppConfig.ChatSDK.onNewMessage.log && console.log(message?.content);
+    });
+
+    console.log("Chat started!");
+  }, [chatSDK]);
+
+  const endChat = useCallback(async () => {
+    if (!chatSDK) {
+      return;
+    }
+
+    try {
+      await chatSDK.endChat();
+    } catch (error: any) {
+      console.error(error);
+    }
+
+    console.log("Chat ended!");
+  }, [chatSDK]);
+
   return (
     <>
       <h1>ChatSDK Sample</h1>
       <AppDetails />
+      <ChatCommands startChat={startChat} endChat={endChat}/>
     </>
   )
 }
--- omnichannel-chat-sdk/react-fluentui-copilot-control/src/configs/AppConfig.ts
+++ omnichannel-chat-sdk/react-fluentui-copilot-control/src/configs/AppConfig.ts
@@ -2,6 +2,9 @@ const AppConfig = {
   ChatSDK: {
     liveChatConfig: {
       log: false // Log the live chat configuration on init
+    },
+    onNewMessage: {
+      log: true
     }
   }
 }
