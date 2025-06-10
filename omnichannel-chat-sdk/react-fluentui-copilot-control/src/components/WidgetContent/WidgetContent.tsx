8bc9b7180ccbf769fd15c871a4f35e5d06b71ac1
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-06-10 11:54:26 -0700

    Add widget container and widget content components

---
+++ omnichannel-chat-sdk/react-fluentui-copilot-control/src/components/WidgetContent/WidgetContent.tsx
@@ -0,0 +1,13 @@
+interface WidgetContentProps {
+  children: React.ReactNode;
+};
+
+const WidgetContent: React.FC<WidgetContentProps> = ({children}) => {
+  return (
+    <div style={{backgroundColor: 'white', width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      {children}
+    </div>
+  );
+};
+
+export default WidgetContent;
