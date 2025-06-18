5e784214f4631e7ede2eb0a80c0362c94eeb9029
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-06-18 08:41:27 -0700

    Use ErrorBoundary component

--- omnichannel-chat-sdk/react-botframework-webchat-control/src/main.tsx
+++ omnichannel-chat-sdk/react-botframework-webchat-control/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+);
