dd72eb7684060a041853fcba0a632570b0ea271c
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-07-09 16:12:45 -0700

    Add style props

--- omnichannel-chat-sdk/react-botframework-webchat-control/src/components/WidgetContainer/WidgetContainer.tsx
+++ omnichannel-chat-sdk/react-botframework-webchat-control/src/components/WidgetContainer/WidgetContainer.tsx
@@ -3,9 +3,10 @@ import AppConfig from "../../configs/AppConfig";
 
 interface WidgetContainerProps {
   children: React.ReactNode;
+  style?: React.CSSProperties;
 };
 
-const WidgetContainer: React.FC<WidgetContainerProps> = ({children}) => {
+const WidgetContainer: React.FC<WidgetContainerProps> = (props) => {
   useEffect(() => {
     if (AppConfig.components.WidgetContainer.log) {
       console.log('WidgetContainer');
@@ -13,8 +14,8 @@ const WidgetContainer: React.FC<WidgetContainerProps> = ({children}) => {
   }, []);
 
   return (
-    <div style={{position: 'absolute', bottom: 20, right: 20, height: 560, width: 350, border: '1px solid rgb(209, 209, 209)', display: 'flex', flexDirection: 'column'}}>
-      {children}
+    <div style={{position: 'absolute', bottom: 20, right: 20, height: 560, width: 350, border: '1px solid rgb(209, 209, 209)', display: 'flex', flexDirection: 'column', ...(props.style || {})}}>
+      {props.children}
     </div>
   )
 };
