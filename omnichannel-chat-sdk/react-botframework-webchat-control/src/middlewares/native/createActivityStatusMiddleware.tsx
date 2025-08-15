9a5c202b94e99409f7fe07cc23bb6e7de87945c5
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-08-15 11:12:19 -0700

    Create basic activity status middleware

---
+++ omnichannel-chat-sdk/react-botframework-webchat-control/src/middlewares/native/createActivityStatusMiddleware.tsx
@@ -0,0 +1,22 @@
+import AppConfig from "../../configs/AppConfig";
+
+const activityStatusMiddleware = () => (next: CallableFunction) => (...args: any) => {
+  AppConfig.WebChat.activityStatusMiddleware.log && console.log(`[activityStatusMiddleware]`);
+  const [card] = args;
+  if (card.activity) {
+    const { activity } = card;
+    AppConfig.WebChat.activityStatusMiddleware.log && console.log(activity);
+  }
+
+  return next(card);
+};
+
+const createActivityStatusMiddleware = () => {
+    if (AppConfig.WebChat.activityStatusMiddleware.disabled) {
+        return;
+    }
+
+    return activityStatusMiddleware;
+};
+
+export default createActivityStatusMiddleware;
